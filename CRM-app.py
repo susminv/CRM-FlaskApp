69ad88f1525d81427d9fbe0ef2733debd158c8f9
unknown <susmin@inapp.com>
2024-09-19 15:24:45 +0530

    Starting over

--- CRM-app.py
+++ CRM-app.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import urllib.parse
+from flask import Flask, redirect, url_for, render_template
+from flask_bootstrap import Bootstrap 
+from flask_sqlalchemy import SQLAlchemy
+import urllib 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, SelectField
+from wtforms.validators import DataRequired
+from flask import session, flash
+from flask_migrate import Migrate
+
+
+app = Flask(__name__) 
+app.config['SECRET_KEY'] = 'some secret key'
+
+boostrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('hom.html')
