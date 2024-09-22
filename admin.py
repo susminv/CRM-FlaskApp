ce605510987ef9485a890a2e88e0753173a82727
jayadevanm <jayadevan.m@inapp.com>
2024-09-23 00:35:40 +0530

    no db

--- admin.py
+++ admin.py
@@ -5,8 +5,9 @@ from flask_sqlalchemy import SQLAlchemy #import SQLAlchemy class from flask_sqla
 from flask_migrate import Migrate
 import urllib.parse
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,IntegerField,DecimalField,SelectField,FloatField,PasswordField,DateField
-from wtforms.validators import DataRequired,Length,EqualTo,Email
+from wtforms import StringField,SubmitField,IntegerField,DecimalField,SelectField,FloatField,PasswordField,DateField,PasswordField
+
+from wtforms.validators import DataRequired,Length,EqualTo,Email,InputRequired
 from flask import session
 from flask_wtf.file import FileField
 from werkzeug.utils import secure_filename
@@ -15,6 +16,7 @@ from flask import flash
 from flask_login import LoginManager,login_user,UserMixin
 from flask_login import current_user,logout_user,login_required
 from email_validator import validate_email, EmailNotValidError
+from datetime import datetime
 
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
@@ -24,17 +26,112 @@ app.config.from_object('config.Config')#loading the configuration from the confi
 mydb_obj = SQLAlchemy(app) #create an SQLAlchemy object
 migrate=Migrate(app,mydb_obj)
 
+class Qualifications(mydb_obj.Model):
+    __tablename__ = 'qualifications'
+    qualification_id = mydb_obj.Column(mydb_obj.Integer, primary_key=True)#PK
+    qualification_name = mydb_obj.Column(mydb_obj.String(150))
+    courseid = mydb_obj.Column(mydb_obj.Integer, mydb_obj.ForeignKey('courses.courseid'),nullable=False) #FK
+
+
+    users = mydb_obj.relationship('user', backref='qualifications', uselist=False)  # One-to-one relationship with Enquiries
+    courses = mydb_obj.relationship('Courses',backref='qualifications', uselist=False)  # One-to-one relationship with Enquiries
+    #user = mydb_obj.relationship('User', back_populates='qualifications', uselist=False)  # One-to-one relationship with Enquiries
+
+#USER Table
+class User(mydb_obj.Model):
+    __tablename__ = 'users'
+    id = mydb_obj.Column(mydb_obj.Integer, primary_key=True) #primary key
+    username = mydb_obj.Column(mydb_obj.String(150), unique=True, nullable=False)
+    password = mydb_obj.Column(mydb_obj.String(150), unique=True, nullable=False)
+    firstname = mydb_obj.Column(mydb_obj.String(150), nullable=False)
+    lastname = mydb_obj.Column(mydb_obj.String(150), nullable=False)
+    gender=mydb_obj.Column(mydb_obj.String(10),nullable=False)
+    age = mydb_obj.Column(mydb_obj.Integer,nullable=False) 
+    email = mydb_obj.Column(mydb_obj.String(150), unique=True, nullable=False)
+    phone_number = mydb_obj.Column(mydb_obj.Integer, unique=True, nullable=False)
+    address = mydb_obj.Column(mydb_obj.String(200), unique=True, nullable=False)
+    date_of_birth = mydb_obj.Column(mydb_obj.Date, nullable=False)  # Date of birth
+    highest_qualificationid = mydb_obj.Column(mydb_obj.Integer, mydb_obj.ForeignKey('qualifications.qualification_id'),nullable=False) #FK
+
+
+    marks = mydb_obj.Column(mydb_obj.Integer, nullable=False)
+    yearofgraduation =  mydb_obj.Column(mydb_obj.Integer, nullable=False)
+    #enquiries = mydb_obj.relationship('Enquiries', backref='user', uselist=False)  # One-to-one relationship with Enquiries
+
+class CourseModules(mydb_obj.Model):
+    __tablename__ = 'coursemodules'
+    moduleid = mydb_obj.Column(mydb_obj.Integer, primary_key=True)#PK
+    modulename = mydb_obj.Column(mydb_obj.String(150))
+    courseid = mydb_obj.Column(mydb_obj.Integer, mydb_obj.ForeignKey('courses.courseid'),nullable=False) #Foreignkey
+
+    coursess = mydb_obj.relationship('Courses', backref='coursemodules')  # One-to-many relationship with Courses
+
+
+#COURSE Table
+class Courses(mydb_obj.Model):
+    __tablename__ = 'courses'
+    courseid = mydb_obj.Column(mydb_obj.Integer, primary_key=True) #primary key
+    coursename= mydb_obj.Column(mydb_obj.String(150), unique=True, nullable=False)
+    description= mydb_obj.Column(mydb_obj.String(150), unique=True, nullable=False)
+    course_duration = mydb_obj.Column(mydb_obj.Integer, nullable=False)  # Duration in hours
+    fees = mydb_obj.Column(mydb_obj.Integer, nullable=False)
+    qualification_id = mydb_obj.Column(mydb_obj.Integer, mydb_obj.ForeignKey('qualifications.qualification_id'),nullable=False) #FK
+    moduleid= mydb_obj.Column(mydb_obj.Integer, mydb_obj.ForeignKey('coursemodules.moduleid'),nullable=False) #Foreignkey
+    #courses = mydb_obj.relationship('Courses', backref='user')
+
+    enquiriess= mydb_obj.relationship('Enquiries', backref='courses', uselist=False)  # One-to-one relationship with courses
+    #qualifications = mydb_obj.relationship('qualifications', backref='courses', uselist=False)  # One-to-one relationship with Enquiries
+    #coursemodules = mydb_obj.relationship('courseModules', backref='courses', uselist=False)  # One-to-one relationship with Enquiries
+
+
+#Resources Table
+class Resources(mydb_obj.Model):
+    __tablename__ = 'resources'
+    resourceid = mydb_obj.Column(mydb_obj.Integer, primary_key=True) #PK
+    resourcename = mydb_obj.Column(mydb_obj.String(150))
+    enquiriess = mydb_obj.relationship('Enquiries', backref='resources', uselist=False)  # One-to-one relationship with Enquiries
+
+#Enquiry Status Table
+class EnquiryStatus(mydb_obj.Model):
+    __tablename__ = 'enquirystatus'
+    enquirys_id = mydb_obj.Column(mydb_obj.Integer, primary_key=True) #PK
+    enquiry_status =  mydb_obj.Column(mydb_obj.Integer)
+    enquiries = mydb_obj.relationship('Enquiries', backref='enquirystatus', uselist=False)  # One-to-one relationship with Enquiries
+
+class Enquiries(mydb_obj.Model):
+    __tablename__ = 'enquiries'
+    enqid = mydb_obj.Column(mydb_obj.Integer, primary_key=True)
+    userid = mydb_obj.Column(mydb_obj.Integer, mydb_obj.ForeignKey('users.id'),nullable=False)
+    courseid = mydb_obj.Column(mydb_obj.Integer, mydb_obj.ForeignKey('courses.courseid'),nullable=False) #FK
+
+    enquiry_date =  mydb_obj.Column(mydb_obj.DateTime, default=datetime.now) 
+    message =  mydb_obj.Column(mydb_obj.String(150))
+    statusid = mydb_obj.Column(mydb_obj.Integer, mydb_obj.ForeignKey('enquirystatus.enquirys_id'),nullable=False) #FK
+    resource = mydb_obj.Column(mydb_obj.Integer, mydb_obj.ForeignKey('resources.resourceid'),nullable=False) #FK
+
+
+
+
+
+
+
+
+
 
 
 class addform(FlaskForm):
-    name=StringField(' Name:',validators=[DataRequired()])
+    firstname=StringField('First Name:',validators=[DataRequired()])
+    lastname=StringField('Last Name:',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=72)])
     username=StringField('Username:',validators=[DataRequired()])
-    age=IntegerField(' age:',validators=[DataRequired()])
+    age=IntegerField(' Age:',validators=[DataRequired()])
     email=StringField(' Email:',validators=[DataRequired(),Email()])
     address=StringField(' Adress:')
     gender=SelectField(' Gender:',choices=[('Male','Male'),('Female','Female')])
     phone=FloatField('Number',validators=[DataRequired()])
+    dob = DateField('Date of Birth:', validators=[DataRequired()])
     qualification=StringField(' Qualification:',validators=[DataRequired()])
+    marks=IntegerField('Marks(%):',validators=[DataRequired()])
     yearofgrad=StringField(' Year of Graduation:',validators=[DataRequired()])
     submit_btn=SubmitField('SUBMIT')
 
@@ -42,6 +139,7 @@ class addform(FlaskForm):
 
 @app.route('/')
 def index():
+    mydb_obj.create_all()
     return render_template('ADMINHOME1.html')
 
 @app.route('/help')
@@ -54,7 +152,10 @@ def policy():
 
 @app.route('/adduser',methods=['GET','POST'])
 def adduser():
-    name=None
+    firstname=None
+    lastname=None
+    password=None
+    marks=None
     username=None
     age=None
     email=None
@@ -63,26 +164,36 @@ def adduser():
     phone=None
     qualification=None
     yearofgrad=None
+    dob=None
     form=addform()
     if form.validate_on_submit():
-        name=form.name.data
+        firstname=form.firstname.data
+        lastname=form.lastname.data
+        password=form.password.data
         username=form.username.data
         age=form.age.data
         phone=form.phone.data
         email=form.email.data
         address=form.address.data
         gender=form.gender.data
-        qualification=None
-        yearofgrad=None
+        marks=form.marks.data
+        qualification=form.qualification.data
+        yearofgrad=form.yearofgrad.data
 
-        flash(f'Data recieved {name},{age},{email},{address} and {phone}')
+        user=User(firstname=firstname,lastname=lastname,age=age,email=email,password=password,username=username,phone_number=phone,address=address,gender=gender,highest_qualificationid=qualification,marks=marks,yearofgraduation=yearofgrad,date_of_birth=dob)
+        mydb_obj.session.add(user)
+        mydb_obj.session.commit()
 
-        return redirect(url_for('list'))
+        flash(f'Data recieved {firstname}')
+
+        return redirect(url_for('listusers'))
     return render_template('adduser.html',form=form)
 
 @app.route('/listusers')
-def listuser():
-    return render_template('listusers.html')
+def listusers():
+    #return render_template('listusers.html')
+    return render_template('listusers.html',all_user=User.query.all())
+
 
 
 class addenquiryform(FlaskForm):
