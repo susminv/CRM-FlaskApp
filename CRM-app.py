ff32113180d5fec3f81e87ab296d694f44edb762
unknown <susmin@inapp.com>
2024-09-22 21:14:15 +0530

    Added a bunch of pages

--- CRM-app.py
+++ CRM-app.py
@@ -1,25 +1,58 @@
-import urllib.parse
-from flask import Flask, redirect, url_for, render_template
-from flask_bootstrap import Bootstrap 
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
-import urllib 
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, SelectField
-from wtforms.validators import DataRequired
-from flask import session, flash
 from flask_migrate import Migrate
-
+from flask_bootstrap import Bootstrap 
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, EmailField, SelectField, PasswordField, SelectMultipleField, SubmitField, widgets, TextAreaField
+from wtforms.validators import DataRequired, Email, Length, EqualTo,Regexp
+from config import Config  # Make sure to import your Config class
+from flask_login import login_user, LoginManager, logout_user, current_user, login_required
+from flask_login import UserMixin
+from flask import session
+from datetime import datetime, timezone
 
 app = Flask(__name__) 
-app.config['SECRET_KEY'] = 'some secret key'
+app.config.from_object('config.Config')
 
 boostrap = Bootstrap(app)
 
-#-----------------------------------------------------------------------------------------------------#
+#---------Database------------#
 
-#---------CLASSES----->
+#---------Form classes--------#
+class RegistrationForm(FlaskForm):
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    birthday = DateField('Birthday', format='%Y-%m-%d', validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[('Male', 'Male'), ('Female', 'Female'), ('Others', 'Others')], validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone Number', validators=[DataRequired()])
+    state = StringField('State of Domicile', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
+    
+    # Multiple qualifications as checkboxes
+    qualifications = SelectMultipleField(
+        'Qualifications',
+        choices=[('10th','10th'),('12th', '12th'), ('Bachelors', 'Bachelors'), ('Masters', 'Masters')],
+        option_widget=widgets.CheckboxInput(),
+        widget=widgets.ListWidget(prefix_label=False)
+    )
+    
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=8, max=20),
+        Regexp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$', 
+               message="Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.")
+    ])
+    confirmpassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+    
+#WTForms for login
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    submit = SubmitField('Login')
 
-#-----------------------------------------------------------------------------------------------------#
+
 
 #============================================# Pre Login #============================================#
 
@@ -41,9 +74,15 @@ def courses():
 
 #============================================# Login or Sign Up options #============================================#
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('Login.html') # login page
+    form = LoginForm()
+    return render_template('Login.html',form=form) # login page
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('Register.html',form=form) # login page
 
 #============================================# User #============================================#
 
@@ -64,11 +103,12 @@ def uprofile():
 
 @app.route('/user/edit')
 def uedit():
-    return render_template('UserEditProfile.html') # Edit user profile
+    form = RegistrationForm()
+    return render_template('UserEditProfile.html',form=form) # Edit user profile
 
 @app.route('/user/delete') #delete user, and go to pre-login home page
 def udel():
-    pass
+    return render_template('UserDelete.html')
 
 
 #============================================# User Enquiry options #============================================#
